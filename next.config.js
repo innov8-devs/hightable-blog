a9ad00025561fbb0e4c3422a7938e8c8f73ef17c
Vision <visiondaniels32@gmail.com>
2022-10-25 13:29:39 +0000

    Initial COmmit

---
+++ next.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  reactStrictMode: true,
+  images: {
+    domains: ['res.cloudinary.com'],
+  },
+  pageExtensions: ['page.tsx', 'page.ts', 'page.jsx', 'page.js'],
+};
---
+++ pages/index.page.tsx
@@ -0,0 +1,5 @@
+function Homepage() {
+  return <h1 className='text-center my-30 text-xl bold'>Our homepage</h1>;
+}
+
+export default Homepage;
